ba6667f9438e146ef640b111027e6d4d636c0bd6
Mike C <mike.choi@live.ca>
2018-09-02 02:08:05 +0000

    Made header/footer partials and included INDEX routing

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// Dependencies
 var bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     express     = require("express"),
     app         = express();
     
+// Middleware config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Mongoose/model config
 var blogSchema = new mongoose.Schema({
     title:  String,
     image:  String,
@@ -16,6 +19,31 @@ var blogSchema = new mongoose.Schema({
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
+
+
+//TEST CREATE
+// Blog.create({
+//     title: "Test blog",
+//     image: "https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+//     body: "This is a test blog demo"
+// });
+
+
+//RESTful routes
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({},function(err, blogs){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog app server has started...");
